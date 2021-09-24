84e8266779acbbdd3fc89956f7604727b0ae93e3
ivwu22 <ivywu6@gmail.com>
2021-09-24 16:28:07 -0400

    finding the best loan rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -14,4 +14,13 @@ public class App
     public static int doubleTheNumber(int number) {
         return 2 * number;
     }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum+=numbers[i];
+        }
+        return sum;
+    }
+
 }
