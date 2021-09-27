472d08daebb7d41a9f8755df1195702c118556b5
ivwu22 <ivywu6@gmail.com>
2021-09-27 12:34:47 -0400

    part of module 5

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,3 +1,17 @@
-public class Finance {
+import java.util.Map;
 
+public class Finance {
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
+    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
+    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+    public final static Map<String, String> commandsToUsage = Map.of(
+            BEST_LOAN_RATES, "usage: bestLoanRates",
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
+    );
+
+    private static boolean validateCommandArguments(String[] args) {
+        return false;
+    }
 }
