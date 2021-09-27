1c2ffcc6e25b53215bdafb03d5453ad3b5af50cd
ivwu22 <ivywu6@gmail.com>
2021-09-27 13:18:51 -0400

    module 6

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -7,7 +7,7 @@ public class Utilities {
             out = Long.parseLong(in);
         }
         catch (NumberFormatException e) {
-           throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program");
+           throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
         }
         return out;
     }
