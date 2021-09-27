e01977528935b5e3e0dfa2dc9d55f4b67b512376
ivwu22 <ivywu6@gmail.com>
2021-09-27 11:19:33 -0400

    part of module 4

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,29 @@
+package com.h2;
+
+public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
+
+    private int getNumberOfPayments() {
+        return termInYears*12;
+    }
+
+    private float getMonthlyInterestRate(){
+        float interestRate;
+        interestRate = (annualRate/100)/12;
+//        interestRate/=12;
+        return interestRate;
+    }
+
+    public void calculateMonthlyPayment() {
+
+    }
+}
